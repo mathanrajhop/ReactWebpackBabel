dbcd5d4aa3fb9dd27ab578080ce370f0b9647918
mathan prakash <mathanrajhop@gmail.com>
2021-11-19 02:04:04 +0530

    completed setup on react apps

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+import App from "./app"
+
+const Root = () => {
+    return (
+        <App />
+    );
+}
+
+ReactDOM.render(<Root />, document.getElementById("root"));
